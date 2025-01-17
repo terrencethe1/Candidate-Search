7715a3dc5b6d867c504e35e02f01ca15cbda0a6b
terrencethe1 <terrence.jsr@gmail.com>
2025-01-16 21:56:24 -0600

    Add Candidate interface,Created an interface for Candidate objects returned by the API, Included fields: login, avatar_url, location, email, company, and bio.

---
+++ src/interfaces/Candidate.interface.tsx
@@ -0,0 +1,9 @@
+// TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    login: string;
+    avatar_url: string;
+    location: string | null;
+    email: string | null;
+    company: string | null;
+    bio: string | null;
+  }
