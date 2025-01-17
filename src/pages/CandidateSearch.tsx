3dcdf83e0d40e67e17c98b405ef08a71323efacd
terrencethe1 <terrence.jsr@gmail.com>
2025-01-16 22:15:34 -0600

    Added Candidate interface with fields: avatar_url, login, location, email, company, and bio.

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,6 +1,16 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 
+interface Candidate {
+  avatar_url: string;
+  login: string;
+  location?: string;
+  email?: string;
+  company?: string;
+  bio?: string;
+}
+
+
 const CandidateSearch = () => {
   return <h1>CandidateSearch</h1>;
 };
